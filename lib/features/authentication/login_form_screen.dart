c9494e62b1397e1b62c77bdb81a7d5de0aa2a58f
Seunghyum <antmgu@gmail.com>
2023-02-25 11:05:17 +0900

    feat: add unfocus on tap scaffold

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -24,52 +24,59 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
     }
   }
 
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Log in")),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size36,
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Log in")),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Gaps.v28,
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Email",
+                    ),
+                    validator: (value) {
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      if (newValue != null) {
+                        formData['email'] = newValue;
+                      }
+                    },
+                  ),
+                  Gaps.v16,
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Password",
+                    ),
+                    validator: (value) {
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      if (newValue != null) {
+                        formData['password'] = newValue;
+                      }
+                    },
+                  ),
+                  Gaps.v28,
+                  GestureDetector(
+                      onTap: _onSubmitTap,
+                      child: const FormButton(disabled: false))
+                ],
+              )),
         ),
-        child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Gaps.v28,
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Email",
-                  ),
-                  validator: (value) {
-                    return null;
-                  },
-                  onSaved: (newValue) {
-                    if (newValue != null) {
-                      formData['email'] = newValue;
-                    }
-                  },
-                ),
-                Gaps.v16,
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Password",
-                  ),
-                  validator: (value) {
-                    return null;
-                  },
-                  onSaved: (newValue) {
-                    if (newValue != null) {
-                      formData['password'] = newValue;
-                    }
-                  },
-                ),
-                Gaps.v28,
-                GestureDetector(
-                    onTap: _onSubmitTap,
-                    child: const FormButton(disabled: false))
-              ],
-            )),
       ),
     );
   }
