b84af80c34d1642193bd71f601fc18edb4855e41
Seunghyum <antmgu@gmail.com>
2023-03-05 14:35:24 +0900

    feat: add nav and post video button to main screen

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:nomadcode_tiktok_clone/constants/gaps.dart';
+import 'package:nomadcode_tiktok_clone/constants/sizes.dart';
+import 'package:nomadcode_tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
+import 'package:nomadcode_tiktok_clone/features/main_navigation/widgets/post_video_button.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onPostVideoButtonTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: ((context) => Scaffold(
+              appBar: AppBar(
+                title: const Text('record video'),
+              ),
+            )),
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(children: [
+        // 안보이지만 앱상에서는 존재하게끔 만들어 주는 위젯
+        Offstage(
+          offstage: _selectedIndex != 0,
+          child: const Center(
+            child: Text("home"),
+          ),
+        )
+      ]),
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.all(Sizes.size12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                NavTab(
+                    text: "Home",
+                    icon: FontAwesomeIcons.house,
+                    selectedIcon: FontAwesomeIcons.house,
+                    isSelected: _selectedIndex == 0,
+                    onTap: () => _onTap(0)),
+                NavTab(
+                    text: "Discover",
+                    icon: FontAwesomeIcons.compass,
+                    selectedIcon: FontAwesomeIcons.solidCompass,
+                    isSelected: _selectedIndex == 1,
+                    onTap: () => _onTap(1)),
+                Gaps.h24,
+                GestureDetector(
+                    onTap: _onPostVideoButtonTap,
+                    child: const PostVideoButton()),
+                Gaps.h24,
+                NavTab(
+                    text: "Inbox",
+                    icon: FontAwesomeIcons.message,
+                    selectedIcon: FontAwesomeIcons.solidMessage,
+                    isSelected: _selectedIndex == 3,
+                    onTap: () => _onTap(3)),
+                NavTab(
+                    text: "Profile",
+                    icon: FontAwesomeIcons.user,
+                    selectedIcon: FontAwesomeIcons.solidUser,
+                    isSelected: _selectedIndex == 4,
+                    onTap: () => _onTap(4)),
+              ],
+            ),
+          )),
+    );
   }
 }
---
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:nomadcode_tiktok_clone/constants/gaps.dart';
+
+class NavTab extends StatelessWidget {
+  final String text;
+  final bool isSelected;
+  final IconData icon;
+  final IconData selectedIcon;
+  final void Function() onTap;
+
+  const NavTab({
+    super.key,
+    required this.text,
+    required this.isSelected,
+    required this.icon,
+    required this.selectedIcon,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          color: Colors.black,
+          child: AnimatedOpacity(
+            opacity: isSelected ? 1 : 0.6,
+            duration: const Duration(milliseconds: 300),
+            child: Column(
+              mainAxisSize: MainAxisSize
+                  .min, // Column 컴포넌트는 기본적으로 최대한의 높이를 차지함. MainAxisSize.min 설정으로 children 만큼의 사이즈를 가지도록 설정 할 수 있음.
+              children: [
+                FaIcon(
+                  isSelected ? selectedIcon : icon,
+                  color: Colors.white,
+                ),
+                Gaps.v5,
+                Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
